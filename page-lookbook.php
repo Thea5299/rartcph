3cb4b5d3812ad7be6fba9b52dd63898279590b2f
thea5299 <thea5299@stud.kea.dk>
2022-05-19 12:57:38 +0200

    godt i gang

---
+++ page-lookbook.php
@@ -0,0 +1,102 @@
+<?php
+get_header();
+?>
+
+<style>
+
+    /* :root {
+        --burgundy: #6B2020; 
+        --rose: #E3C0C0;
+        --navy: #062360;
+        --lightblue: #a0c1ff;
+    }
+   */
+
+  #sec_1 {
+        height: 500px;
+        background-image: url("https://designtree.dk/rartcph/wp-content/themes/Divi-child/img/lookbooksplash.png");
+    background-repeat: no-repeat;
+  /* background-attachment: fixed; */
+  background-position: center;
+    }
+</style>
+
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Epilogue:wght@300;400&display=swap" rel="stylesheet"> 
+
+<section id="body">
+<section id="sec_1" class="d-flex justify-content-center align-items-center">
+      <div class="splash container-fluid">
+          <div class="row">
+              <div class="col">
+                  <h1 class="headline text-center">LOOKBOOK</h1>
+                  <p class="sub-header text-center fs-5">- Inspiration for your specialmade rug or mirror</p>
+              </div>
+          </div>
+      </div>
+      </section>
+
+      <section id="sec_2" class="col pt-5">
+          <div class="row">
+          <div class="col-12 text-center">
+          <h2>H2</h2>
+          </div>
+          <div class="col-12 text-center">
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos delectus ipsum quidem laboriosam temporibus ducimus dolorem tempore blanditiis dolores, accusamus iste quibusdam dolore officiis ab modi recusandae doloribus totam minima!</p>
+          </div>
+          </div>
+      </section>
+
+      <section id="sec_3" class="col pt-5">
+          <div class="row">
+          <div class="col-12 col-md-8">
+          <img src="https://designtree.dk/rartcph/wp-content/themes/Divi-child/img/aboutrartbackgroundwithoverlay.png" alt="tuftedrug">
+          </div>
+          <div class="col-12 col-md-4">
+          <img src="https://designtree.dk/rartcph/wp-content/themes/Divi-child/img/aboutrartbackgroundwithoverlay.png" alt="tuftedrug">
+          </div>
+          </div>
+      </section>
+
+      <section id="sec_4" class="col">
+          <div class="row">
+          <div class="col-12 col-md-4">
+          <img src="https://designtree.dk/rartcph/wp-content/themes/Divi-child/img/aboutrartbackgroundwithoverlay.png" alt="tuftedrug">
+          </div>
+          <div class="col-12 col-md-4">
+          <img src="https://designtree.dk/rartcph/wp-content/themes/Divi-child/img/aboutrartbackgroundwithoverlay.png" alt="tuftedrug">
+          </div>
+          <div class="col-12 col-md-4">
+          <img src="https://designtree.dk/rartcph/wp-content/themes/Divi-child/img/aboutrartbackgroundwithoverlay.png" alt="tuftedrug">
+          </div>
+      </section>
+
+      <section id="sec_5" class="col">
+          <div class="row">
+          <div class="col-12 col-md-6">
+          <img src="https://designtree.dk/rartcph/wp-content/themes/Divi-child/img/aboutrartbackgroundwithoverlay.png" alt="tuftedrug">
+          </div>
+          <div class="col-12 col-md-6">
+          <img src="https://designtree.dk/rartcph/wp-content/themes/Divi-child/img/aboutrartbackgroundwithoverlay.png" alt="tuftedrug">
+          </div>
+      </section>
+</section>
+     
+
+
+
+
+    <!-- Option 1: Bootstrap Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/3.8.0/gsap.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/3.8.0/ScrollTrigger.min.js"></script>
+
+
+<?php
+get_footer();
+?>
